317a03f8a7dd6602eaab147c0d432b84434d87a9
viktormark <viktormarkovetskyi@gmail.com>
2023-07-30 13:59:39 +0300

    add files

---
+++ BasicSelenium/new_window.py
@@ -0,0 +1,9 @@
+import pytest as pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import math
+from selenium.webdriver.support.ui import Select
+import os
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
